a40153b26e18dc884d6dfffb4178b2df1990c331
Noe Blanquart <blanquartnoe@gmail.com>
2026-01-15 11:33:32 +0100

    clean repo

--- assets/js/swipe.js
+++ assets/js/swipe.js
@@ -2,7 +2,7 @@ const avatarChoisi = localStorage.getItem('avatar');
 const avatarImg = document.getElementById('avatar-selected');
 
 if (avatarChoisi && avatarImg) {
-    avatarImg.src = `../assets/images/${avatarChoisi}.jpg`;
+    avatarImg.src = `../assets/images/${avatarChoisi}.png`;
 }
 
 const baseProfiles = [
